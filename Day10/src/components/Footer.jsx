7128d2acdee15082de387967cc45c26cb31ba05b
SnehaSaras <126309505+SnehaSaras@users.noreply.github.com>
2023-09-27 14:49:52 +0530

    Add files via upload

---
+++ Day10/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './admin/Dashboard/Dashboard.css'
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+      return( 
+            <div className='footer'>
+                  <Link className='f'>CopyRight BY SNEHA K</Link>
+            </div>
+      )
+}
+
+export default Footer

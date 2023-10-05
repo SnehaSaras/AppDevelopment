f603e7c8c3eae64183513b71663ad4ef41edbe3e
SnehaSaras <126309505+SnehaSaras@users.noreply.github.com>
2023-10-05 14:54:16 +0530

    Add files via upload

---
+++ Day12/src/main/java/com/codemancer/sneha/service/impl/AuthServiceImpl.java
@@ -0,0 +1,61 @@
+package com.codemancer.sneha.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.codemancer.sneha.dto.request.AuthenticationRequest;
+import com.codemancer.sneha.dto.request.RegisterRequest;
+import com.codemancer.sneha.dto.response.AuthenticationResponse;
+import com.codemancer.sneha.model.User;
+import com.codemancer.sneha.model.enumerate.Role;
+import com.codemancer.sneha.repository.UserRepository;
+import com.codemancer.sneha.service.AuthService;
+import com.codemancer.sneha.util.JwtUtil;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public boolean userRegistration(RegisterRequest request) {
+        Optional<User> isUserExists = userRepository.findByEmail(request.getEmail());
+        if (!isUserExists.isPresent()) {
+            var user = User.builder()
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .isEnabled(true)
+                    .role(Role.valueOf(request.getRole().toUpperCase()))
+                    .build();
+            userRepository.save(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public AuthenticationResponse userAuthentication(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var token = jwtUtil.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .email(user.getEmail())
+                .build();
+    }
+}
